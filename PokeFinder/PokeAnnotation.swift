91017afb7cc26ba75ab6985c165305ce9e86c052
Igor Dorovskikh <i.dorovskikh@agilefusion.com>
2016-10-10 17:24:35 -0700

    first commit

---
+++ PokeFinder/PokeAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  PokeAnnotation.swift
+//  PokeFinder
+//
+//  Created by Mark Price on 7/25/16.
+//  Copyright © 2016 Devslopes. All rights reserved.
+//
+
+import Foundation
---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Mark Price on 7/25/16.
+//  Copyright © 2016 Devslopes. All rights reserved.
+//
+
+#ifndef PokeFinder_Bridging_Header_h
+#define PokeFinder_Bridging_Header_h
+
+
+#endif /* PokeFinder_Bridging_Header_h */
